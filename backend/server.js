799ea83064540da3c624b00a35a7a1f67143f69d
Prajapati_Darshan <vp941334@gmail.com>
2026-07-03 11:20:45 +0530

    Fix endpoints

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://job-portal-8j14.onrender.com",
     credentials: true,
 };
 
